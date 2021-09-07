7f388bc16efcccde0c3a84a98983cefc43511339
Mikita <Kuznikit@gmail.com>
2021-09-07 23:56:29 +0300

    Test ZipCode Added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,8 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
@@ -10,6 +13,11 @@ public class BaseTest {
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        //options.addArguments("headless");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
