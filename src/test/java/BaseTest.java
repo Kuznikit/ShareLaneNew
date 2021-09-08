121505f2bc4ceeb0f922816438ade43613c4e6de
Mikita <Kuznikit@gmail.com>
2021-09-08 17:11:07 +0300

    Test ZipCode Added+SignUp-added+

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,4 +1,5 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
+
     @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().setup();
@@ -18,7 +20,9 @@ public class BaseTest {
         //options.addArguments("headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
     }
+
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
         driver.quit();
--- src/test/java/ShareLaneTest.java
+++ src/test/java/ShareLaneTest.java
@@ -1,10 +1,10 @@
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class ShareLaneTest extends BaseTest {
+
     @Test
     public void LinkSignUpShouldWork() {
         driver.get("https://www.sharelane.com/cgi-bin/main.py");
@@ -46,5 +46,86 @@ public class ShareLaneTest extends BaseTest {
         driver.findElement(By.cssSelector("[value=Continue]")).click();
         String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
         assertEquals(error, "Oops, error on page. ZIP code should have 5 digits", "text is not correct");
+    }
+
+    @Test
+    public void FirstNameWithOutTextShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+
+    @Test
+    public void FirstNameWithNumberShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("123");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+
+    @Test
+    public void FirstNameWithCyrillicShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("ЕЁиы");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String errorSingUp = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(errorSingUp, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+
+    @Test
+    public void EmailWithOutTextShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("email")).sendKeys("");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+    @Test
+    public void EmailWithWrongTypeShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("email")).sendKeys("abc");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+    @Test
+    public void PassWithOutTextShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("password1")).sendKeys("");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+    @Test
+    public void PassLessThan4SymbolsShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("password1")).sendKeys("111");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+    @Test
+    public void Pass2DoesntMatchPass1ShouldRejected() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password1")).sendKeys("1112");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String error = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        assertEquals(error, "Oops, error on page. Some of your fields have invalid data or email was previously used", "First Name should have any symbols");
+    }
+    @Test
+    public void RegisterIsWorking() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("abc");
+        driver.findElement(By.name("email")).sendKeys("abc@gmail.com");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        driver.findElement(By.cssSelector("[value=Register]")).click();
+        String isSignUpOpened = driver.findElement(By.cssSelector("[class='confirmation_message']")).getText();
+        assertEquals(isSignUpOpened, "Account is created!");
+
     }
 }
